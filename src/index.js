f4e5b2b1a70c68df37ede87c305db569c3573cac
Anton.Koptelov <Anton.Koptelov@billing.ru>
2019-08-14 21:14:57 +0400

    add cart

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 'use strict';
 
-//чекбокс
+// start чекбокс
 const checkboxes = document.querySelectorAll('.filter-check_checkbox');
 
 checkboxes.forEach(function (checkbox) {
@@ -14,3 +14,50 @@ checkboxes.forEach(function (checkbox) {
 });
 
 // end чекбокс
+
+// start корзина
+const modalCart = document.querySelector('.cart'),
+    btnOpenCart = document.getElementById('cart'),
+    btnCloseCart = modalCart.querySelector('.cart-close');
+
+// кнопка открыть корзину
+btnOpenCart.addEventListener('click', () => {
+    modalCart.style.display = 'flex';
+    document.body.style.overflow = 'hidden';
+});
+
+// кнопка закрыть корзину
+btnCloseCart.addEventListener('click', () => {
+    modalCart.style.display = 'none';
+    document.body.style.overflow = '';
+});
+
+// end корзина
+
+// start добавление товара
+const cards = document.querySelectorAll('.goods .card'),
+    cartWrapper = document.querySelector('.cart-wrapper'),
+    cartEmpty = document.getElementById('cart-empty'),
+    countGoods = document.querySelector('.counter');
+
+cards.forEach((card) => {
+   const btn = card.querySelector('button');
+
+   btn.addEventListener('click', () => {
+       const cardClone = card.cloneNode(true);
+       cartWrapper.appendChild(cardClone);
+       cartEmpty.remove();
+       showData();
+   })
+});
+
+function showData() {
+    const cardsCart = cartWrapper.querySelectorAll('.card');
+    countGoods.textContent = cardsCart.length;
+}
+
+// end
+
+// start
+
+// end
